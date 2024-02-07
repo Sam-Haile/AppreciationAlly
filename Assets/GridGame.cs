cec3b2d7d561d14d36938243656ed1c834766fe6
Sam-Haile <92647194+Sam-Haile@users.noreply.github.com>
2024-02-07 18:47:59 -0500

    Push 1

--- Assets/GridGame.cs
+++ Assets/GridGame.cs
@@ -5,13 +5,30 @@ using UnityEngine.UI;
 
 public class GridGame : MonoBehaviour
 {
-    public Image gridImage;
-    private
+    public GameObject gridImage;
+    public Canvas canvas;
 
-    
-    private void Update()
+    private int screenWidth;
+    private int screenHeight;
+
+    private void Start()
     {
+        screenWidth = Screen.width; 
+        screenHeight = Screen.height;
+
+        Spawn();
+    }
+
+
+    private void Spawn()
+    {
+        Vector3 spawnPosition = new Vector3(0, Camera.main.orthographicSize + gridImage.GetComponent<SpriteRenderer>().bounds.size.y / 2, 0);
         
+        GameObject spawnedBackground = Instantiate(gridImage, spawnPosition, Quaternion.identity);
+
+        float randomScale = Random.Range(.45f, 1f);
+
+        spawnedBackground.transform.localScale = new Vector3(randomScale, randomScale, randomScale);
     }
 
 }
