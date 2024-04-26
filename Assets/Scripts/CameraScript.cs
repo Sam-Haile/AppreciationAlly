36428795300837f94dda742a726f6c6a907ddf84
Samuel Haile <92647194+Sam-Haile@users.noreply.github.com>
2024-04-25 20:45:41 -0400

    fixing camera mirroring issues

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -15,10 +15,9 @@ public class CameraScript : MonoBehaviour
     void Start()
     {
         devices = WebCamTexture.devices;
-        camTexture = new WebCamTexture();
     }
 
-    void UpdateCameraRender()
+    void UpdateCameraRender(bool mirror)
     {
         if (camTexture.width < 100)
         {
@@ -27,12 +26,23 @@ public class CameraScript : MonoBehaviour
 
         float aspectRatio = (float)camTexture.width / (float)camTexture.height;
         imageFitter.aspectRatio = aspectRatio;
-        image.uvRect = camTexture.videoVerticallyMirrored ? new Rect(1, 0, -1, 1) : new Rect(0, 0, 1, 1);
+
+        if (mirror)
+        {
+            image.uvRect = new Rect(1, 0, -1, 1); 
+
+        }
+        else
+        {
+            image.uvRect = new Rect(0, 0, 1, 1);
+
+        }
     }
 
     void Update()
     {
-        image.transform.rotation = baseRotation * Quaternion.AngleAxis(camTexture.videoRotationAngle, Vector3.up);
+        //if(camTexture != null)
+          //image.transform.rotation = baseRotation * Quaternion.AngleAxis(camTexture.videoRotationAngle, Vector3.up);
     }
 
 
@@ -42,31 +52,57 @@ public class CameraScript : MonoBehaviour
         photo.SetPixels(camTexture.GetPixels());
         photo.Apply();
 
+        Texture2D rotatedImg = RotateTexture(photo, 90);
+
         // Encode texture into PNG
-        byte[] bytes = photo.EncodeToPNG();
+        byte[] bytes = rotatedImg.EncodeToPNG();
         string filename = Path.Combine(Application.persistentDataPath, "capturedImage_" + System.DateTime.Now.ToString("yyyyMMddHHmmss") + ".png");
         File.WriteAllBytes(filename, bytes);
 
         Destroy(photo); // Free memory
-
-        Debug.Log("Captured photo saved as: " + filename);
+        Destroy(rotatedImg);
 
         // Optionally, update your gallery immediately
         imgManager.LoadUserImagesFromPersistentData();
         imgManager.RefreshImagesUI();
     }
 
+    private Texture2D RotateTexture(Texture2D originalTexture, float angle)
+    {
+        int width = originalTexture.width;
+        int height = originalTexture.height;
+
+        Texture2D rotatedTexture = new Texture2D(height, width);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                rotatedTexture.SetPixel(y, width - x - 1, originalTexture.GetPixel(x, y));
+            }
+        }
+
+        rotatedTexture.Apply();
+        return rotatedTexture;
+
+    }
+
+
+
     public void OnCameraClick()
     {
         if (WebCamTexture.devices.Length > 0)
         {
+            camTexture = new WebCamTexture();
             image.texture = camTexture;
             camTexture.filterMode = FilterMode.Trilinear;
             camTexture.Play();
-            UpdateCameraRender();
+            UpdateCameraRender(true);
         }
 
     }
+
+
 
     public void ReverseCamera()
     {
@@ -78,9 +114,14 @@ public class CameraScript : MonoBehaviour
                 camTexture.Stop();
                 camTexture.deviceName = (camTexture.deviceName == devices[0].name) ? devices[1].name : devices[0].name;
                 camTexture.Play();
+                UpdateCameraRender(false);
             }
 
         }
     }
 
+    public void TurnCameraOff()
+    {
+        camTexture.Stop();
+    }
 }
