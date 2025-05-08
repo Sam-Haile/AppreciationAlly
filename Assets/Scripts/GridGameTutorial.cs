93d8c03def127280176678db172ec80f506a4d08
MaxWhite2024 <mjwhite@scu.edu>
2025-05-08 19:21:32 -0400

    Changed Grid game tutorial text

--- Assets/Scripts/GridGameTutorial.cs
+++ Assets/Scripts/GridGameTutorial.cs
@@ -25,10 +25,11 @@ public class GridGameTutorial : MonoBehaviour
         TutorialStep step0 = new TutorialStep("Welcome to the Grid Game", 0);
         TutorialStep step1 = new TutorialStep("Here you can practice appreciation", 1);
         TutorialStep step2 = new TutorialStep("To start, select how many sets of images you would like to play through", 2);
-        TutorialStep step3 = new TutorialStep("Then, play by selecting which of the 4 images you are most grateful for", 3);
+        TutorialStep step3 = new TutorialStep("Then, play by touching the image that makes you feel the most grateful", 3);
         TutorialStep step4 = new TutorialStep("Take a moment after each selection to reflect on what you are grateful for", 4);
-        TutorialStep step5 = new TutorialStep("Come back every day to practice some appreciation", 5);
-        TutorialStep step6 = new TutorialStep("Come back every day to practice some appreciation", 6);
+        TutorialStep step5 = new TutorialStep("Come back every day to train your brain", 5);
+        TutorialStep step6 = new TutorialStep("You can personalize the grid game in the settings by adding your own pictures", 6);
+        //TutorialStep step6 = new TutorialStep("You can personalize the grid game in the settings by adding your own pictures", 6);
 
         step0.NextStep = step1;
         step1.NextStep = step2;
